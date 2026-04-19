32206d826db4a770cba8de4a2603714b1446942d
Mythresh <bmythresh2006@gmail.com>
2026-04-19 12:01:30 +0530

    fixed html structure

--- Public/script.js
+++ Public/script.js
@@ -1,3 +1,5 @@
+let cart = {};
+
 async function loadProducts() {
   const res = await fetch("/api/products");
   const products = await res.json();
@@ -19,17 +21,59 @@ async function loadProducts() {
       <h3>${p.name}</h3>
       <p>₹${p.price}</p>
       <p class="strike">₹${p.originalPrice}</p>
-      <button>Add</button>
+
+      <div class="qty">
+        <button onclick="decrease(${p.id})">-</button>
+        <span id="qty-${p.id}">0</span>
+        <button onclick="increase(${p.id}, ${p.price})">+</button>
+      </div>
     `;
 
     container.appendChild(div);
   });
+}
 
-  // categories
-  const categories = [...new Set(products.map(p => p.category))];
-  const catDiv = document.getElementById("categories");
+function increase(id, price) {
+  if (!cart[id]) cart[id] = { qty: 0, price: price };
 
-  catDiv.innerHTML = "All " + categories.join(" ");
+  cart[id].qty++;
+  document.getElementById(`qty-${id}`).innerText = cart[id].qty;
+
+  updateTotal();
+}
+
+function decrease(id) {
+  if (!cart[id] || cart[id].qty === 0) return;
+
+  cart[id].qty--;
+  document.getElementById(`qty-${id}`).innerText = cart[id].qty;
+
+  updateTotal();
+}
+
+function updateTotal() {
+  let total = 0;
+
+  Object.values(cart).forEach(item => {
+    total += item.qty * item.price;
+  });
+
+  document.getElementById("total").innerText = total;
 }
 
 loadProducts();
+function checkout() {
+  let total = document.getElementById("total").innerText;
+
+  if (total == 0) {
+    alert("Cart is empty");
+    return;
+  }
+
+  const upiID = "yourname@upi"; // change this
+  const name = "Student Saver";
+
+  const url = `upi://pay?pa=${upiID}&pn=${name}&am=${total}&cu=INR`;
+
+  window.location.href = url;
+}
